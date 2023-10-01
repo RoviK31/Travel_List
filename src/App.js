d28825f929ca456933639e761c56dd953798b112
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-10-01 22:14:20 +0800

    add sort functionality

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const initialItems = [
 ];
 
 export default function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
@@ -87,10 +87,19 @@ function Form({ onAddItems }) {
   );
 }
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input")
+
+  let sortedItems;
+  if(sortBy === 'input') sortedItems = items
+
+  if(sortBy === 'description') sortedItems = items.slice().sort((a,b)=> a.description.localeCompare(b.description))
+
+  if(sortBy === 'packed') sortedItems = items.slice().sort((a,b)=> Number(a.packed) - Number(b.packed))
+
   return (
     <div className="list">
       <ul>
-        {items.map((item, i) => (
+        {sortedItems.map((item, i) => (
           <Item
             onDeleteItem={onDeleteItem}
             item={item}
@@ -99,6 +108,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={e=> setSortBy(e.target.value)}> 
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
